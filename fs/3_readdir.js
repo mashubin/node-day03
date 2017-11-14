3b36c0700dabaeeede54e82b5ff1054fc35d9276
mashubin <32285084+mashubin@users.noreply.github.com>
2017-11-14 11:37:45 +0800

    gai

---
+++ fs/3_readdir.js
@@ -0,0 +1,8 @@
+/**
+ * Created by web-01 on 2017/11/14.
+ */
+const fs =require('fs')
+fs.readdir(__dirname,(err,files)=>{
+    if(err) throw err
+    console.log(files);
+})
---
+++ fs/4_writefile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by web-01 on 2017/11/14.
+ */
+const fs =require('fs')
+const path= require('path')
+fs.writeFile(path.join(__dirname,'/test.txt'),'data new.....',(err)=>{
+    if(err) throw err
+})
