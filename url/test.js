c93a73cd7898a3ec40a265996821228bdd6a9204
mashubin <32285084+mashubin@users.noreply.github.com>
2017-11-14 10:01:37 +0800

    the first

---
+++ url/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by web-01 on 2017/11/14.
+ */
+const url = require("url");
+let string ='http://bing.com:80/test1/test2?k1=v1&k2=v2#anch';
+console.log(url.parse(string,true));
+console.log(url.format(url.parse(string)));
